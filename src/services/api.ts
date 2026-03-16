98f0a36041a621a28500391e8bfa701190a97eae
ayoubMO19 <ayoubweb04@gmail.com>
2026-03-16 14:31:20 +0100

    new changes

--- src/services/api.ts
+++ src/services/api.ts
@@ -19,7 +19,7 @@ import type {
 } from "../types/api";
 
 // Base URL from environment variables
-const API_BASE_URL = import.meta.env.API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 // Create axios instance
 export const api = axios.create({
